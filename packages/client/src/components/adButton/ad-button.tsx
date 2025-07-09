408f54b02148b7869eab9d0f5081ea6d72f4f039
Sofia.Povarnitsyna <spo@item.expert>
2025-07-09 22:31:09 +0300

    feat: вёрстка страницы ошибок 400
    - доработала стили 404й страницы
    - доработала клики по кнопкам

--- packages/client/src/components/adButton/ad-button.tsx
+++ packages/client/src/components/adButton/ad-button.tsx
@@ -1,17 +1,38 @@
 import Button from '@mui/material/Button'
 import './style.scss'
 
+import { useNavigate } from 'react-router-dom'
+
 interface AdButtonProps {
   variant?: 'text' | 'outlined' | 'contained'
   text?: string
+  to?: string // внутренняя навигация
+  href?: string // внешняя ссылка
+  onClick?: () => void // обработчик клика
 }
 
 export default function AdButton({
   variant = 'contained',
   text = '',
+  to,
+  href,
+  onClick,
 }: AdButtonProps) {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick()
+    } else if (to) {
+      navigate(to)
+    } else if (href) {
+      window.location.href = href
+    }
+    // иначе ничего не делаем
+  }
+
   return (
-    <Button className="adButton" variant={variant}>
+    <Button onClick={handleClick} className="adButton" variant={variant}>
       {text}
     </Button>
   )
