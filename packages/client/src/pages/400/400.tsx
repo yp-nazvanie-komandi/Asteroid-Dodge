62d83bb09dd838fc20d4124bcca6e149418aa426
Sofia.Povarnitsyna <spo@item.expert>
2025-07-09 22:31:09 +0300

    feat: вёрстка страницы ошибок 400
    - доработала стили 404й страницы
    - доработала клики по кнопкам

--- packages/client/src/pages/400/400.tsx
+++ packages/client/src/pages/400/400.tsx
@@ -7,8 +7,8 @@ const Error400: React.FC = () => {
     <div className={'container container--error'}>
       <h1 className={'error'}>404</h1>
       <p>Ой, что то пошло не так</p>
-      <AdButton text={'Go back'} />
-      <AdButton text={'Main Page'} />
+      <AdButton text={'Go back'} to="-1" />
+      <AdButton text={'Main Page'} to="/" />
     </div>
   )
 }
