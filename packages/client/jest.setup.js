c7a11a3f527fd4b8ab5bda906f195c2d2d452da2
Sofia.Povarnitsyna <spo@item.expert>
2025-07-10 20:30:12 +0300

    refactor(компонент кнопки): поправила логику для прохождения тестов

--- packages/client/jest.setup.js
+++ packages/client/jest.setup.js
@@ -1,4 +1,5 @@
-import { TextEncoder, TextDecoder } from 'util';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { TextEncoder, TextDecoder } = require('util');
 
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
+globalThis.TextEncoder = TextEncoder;
+globalThis.TextDecoder = TextDecoder;
