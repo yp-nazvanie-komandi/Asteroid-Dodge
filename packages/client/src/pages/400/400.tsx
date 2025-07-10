3199b343eb37ad94e0478f2191ffa23fd594a9b5
Sofia.Povarnitsyna <spo@item.expert>
2025-07-10 20:09:58 +0300

    style(комопонент кнопки): поправила вызов компонента кнопки

--- packages/client/src/pages/400/400.tsx
+++ packages/client/src/pages/400/400.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-import AdButton from '../../components/AdButton/AdButton'
+import Button from '../../components/Button/Button'
 
 const Error400 = () => {
   return (
     <div className={'container container--error'}>
       <h1 className={'error'}>404</h1>
       <p>Ой, что то пошло не так</p>
-      <AdButton text={'Go back'} to="-1" />
-      <AdButton text={'Main Page'} to="/" />
+      <Button text={'Go back'} to="-1" />
+      <Button text={'Main Page'} to="/" />
     </div>
   )
 }
