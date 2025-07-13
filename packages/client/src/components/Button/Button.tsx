dc47dc433c76d7facb36326b4edbb32e7bdd6a3f
Sofia.Povarnitsyna <spo@item.expert>
2025-07-13 17:03:35 +0300

    refactor(компонент кнопки): сделала ее более тупой, вынесла клик вне кнопки

--- packages/client/src/components/Button/Button.tsx
+++ packages/client/src/components/Button/Button.tsx
@@ -1,35 +1,15 @@
 import ButtonMaterial from '@mui/material/Button'
 import './style.scss'
 
-import { useNavigate } from 'react-router-dom'
 import React from 'react'
 import { ButtonPropsExtended } from '../../types/button'
 
 export default function Button({
   variant = 'contained',
   text = '',
-  to,
-  href,
-  onClick,
 }: ButtonPropsExtended) {
-  const navigate = useNavigate()
-
-  const handleClick = () => {
-    if (onClick) {
-      onClick()
-    } else if (to) {
-      navigate(to)
-    } else if (href) {
-      window.location.href = href
-    }
-    // иначе ничего не делаем
-  }
-
   return (
-    <ButtonMaterial
-      onClick={handleClick}
-      className="adButton"
-      variant={variant}>
+    <ButtonMaterial className="adButton" variant={variant}>
       {text}
     </ButtonMaterial>
   )
--- packages/client/src/types/button.tsx
+++ packages/client/src/types/button.tsx
@@ -3,7 +3,4 @@ import { ButtonProps } from '@mui/material/Button/Button'
 export type ButtonPropsExtended = ButtonProps & {
   variant?: 'text' | 'outlined' | 'contained'
   text?: string
-  to?: string // внутренняя навигация
-  href?: string // внешняя ссылка
-  onClick?: () => void // обработчик клика
 }
