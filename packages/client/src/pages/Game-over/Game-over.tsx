dd6ac18515125a21359230779677b2fcd1276e2d
Sofia.Povarnitsyna <spo@item.expert>
2025-07-16 18:01:59 +0300

    feat(страница конца игры): закончила с версткой

--- packages/client/src/pages/Game-over/Game-over.tsx
+++ packages/client/src/pages/Game-over/Game-over.tsx
@@ -1,10 +1,33 @@
 import Button from '../../components/Button/Button'
+import { useNavigate } from 'react-router'
 
 export const GameOver = () => {
+  const navigate = useNavigate()
+  const countPoints = 777
+
   return (
     <div className={'container container--game-over'}>
-      <Button text={'Play again'} />
-      <Button text={'Main menu'} />
+      <h1 className="title title--white">GAME OVER</h1>
+
+      <p> Ваш счет: {countPoints}</p>
+
+      <Button
+        color={'info'}
+        size={'large'}
+        onClick={() => {
+          navigate('/start')
+        }}
+        text={'Play again'}
+      />
+
+      <Button
+        color={'info'}
+        size={'large'}
+        onClick={() => {
+          navigate('/')
+        }}
+        text={'Main menu'}
+      />
     </div>
   )
 }
