8b67596a7162d43f3c335f24aeafe91e91fa48af
Daniil Salip <welpodron@gmail.com>
2025-08-01 10:29:44 +0300

    fix: error and 404 require auth (#97)

--- packages/client/src/components/Router/Router.tsx
+++ packages/client/src/components/Router/Router.tsx
@@ -20,12 +20,11 @@ export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="registration" element={<Registration />} />
         <Route index element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
         <Route path="/" element={<PrivateRoutes />}>
           <Route path="profile" element={<Profile />} />
-          {/* TODO: при необходимости можем добавить отдельными роутами старт экран и конец экран игры и вынести game как родителя */}
           <Route path="game" element={<GameCanvas />} />
           <Route path="start" element={<Start />} />
           <Route path="game-over" element={<GameOver />} />
@@ -35,9 +34,9 @@ export const Router = () => {
             <Route path="new" element={<CreateForumTopic />} />
             <Route path=":topicId" element={<ForumTopic />} />
           </Route>
-          <Route path="/error" element={<Exception />} />
-          <Route path="*" element={<Error400 />} />
         </Route>
+        <Route path="/error" element={<Exception />} />
+        <Route path="*" element={<Error400 />} />
       </Routes>
     </BrowserRouter>
   )
